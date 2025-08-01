f5ec7c41a11c96c225b546894b798066348db780
20031907-DBS <196089731+20031907-DBS@users.noreply.github.com>
2025-08-01 13:46:57 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 #CORS(app, supports_credentials=True)
-CORS(app, supports_credentials=True, origins=["http://localhost:3000", "http://localhost:3001"])
+CORS(app, supports_credentials=True)
 #socketio = SocketIO(app, cors_allowed_origins="*")
-socketio = SocketIO(app, cors_allowed_origins=["http://localhost:3000", "http://localhost:3001"])
+socketio = SocketIO(app)
 
 # Initialize extensions
 db.init_app(app)
